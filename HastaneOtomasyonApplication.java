be78a93114825cf6ada2660c52c808aa0d527126
aleynademirtass <129094406+aleynademirtass@users.noreply.github.com>
2024-12-24 01:11:51 +0300

    Add files via upload

---
+++ HastaneOtomasyonApplication.java
@@ -0,0 +1,19 @@
+package example.hastaneotomasyon;
+
+import example.hastaneotomasyon.Observer.DoktorObserver;
+import example.hastaneotomasyon.Observer.HastaObserver;
+import example.hastaneotomasyon.Subject.Subject;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HastaneOtomasyonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HastaneOtomasyonApplication.class, args);
+    }
+
+
+
+
+}
